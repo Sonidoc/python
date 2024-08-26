582047ab93f80be5e258f672e648e597a3266cd8
Sonidoc <pablo.anderssonmelia@elev.ga.ntig.se>
2024-08-26 14:34:05 +0200

    uppgift1

---
+++ modol1.py
@@ -0,0 +1,32 @@
+print("hello world") #enstaka rad av kommentar
+
+print("citat: \"Jävla råtta\" ")
+
+print("citat:\"Lorem Ipsum\" ")
+
+
+print("""""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+""")
+
+print("Bablo\nBandersson Belian")
+
+print("Fy"+ "17!")      
+
+name = input("Vad är mitt namn? Jag glömde det.")
+
+print(name +"," + " Just det, det är mitt namn ")
+
+name = input("Vad du heta?:")
+
+print("Välkommen!" + name)
---
+++ modul2.py
@@ -0,0 +1,7 @@
+print("=" * 20)
+name = input("Skriv namn: ") #input är alltid standard text (sträng)
+
+
+print("Välkommen \"" + name + "\"")
+
+""input tar in data från användaren av programmet och den sparar det i en variabel som är en sträng"""
