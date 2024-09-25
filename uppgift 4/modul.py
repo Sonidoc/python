af76c5cd88cab812e7660a9ef5e7a96cadb4394e
Sonidoc <pablo.anderssonmelia@elev.ga.ntig.se>
2024-09-25 12:22:53 +0200

    modul4

---
+++ uppgift 4/modul.py
@@ -0,0 +1,49 @@
+import os
+os.system('cls')
+
+while True:
+    letter = input("Te voy a golpear con mi polla, para que sientes la verdadera esencia ajajajajajaaja. 7 palabras, E S E N C I A.").upper()
+
+    if letter.lower() == "q":
+        break
+    else:
+        print(letter)
+    
+
+något = input("Säg något:")   
+for x in range(10):
+    print(något)
+
+
+
+tablos = int(input("escribe un numero entre 1 y 10:"))
+
+while tablos < 11:
+    print(tablos)
+    tablos += 1
+
+
+Gabriel = int(input("säg ett number som jag ska räkna till  :"))
+
+boblas = 1 
+
+while True: 
+    boblas < Gabriel
+    print(boblas)
+    boblas += 1
+    if boblas == Gabriel:
+        break 
+
+   
+    
+
+
+
+
+
+#    apor = ["pablo","tablos","benjamin"]
+#    for x in apor:
+#         print(x)
+#      if x == "tablos":
+#           break
+ 
